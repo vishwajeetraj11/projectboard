609e3e53a0f2fdea2ae3c410ce02d5c7b6fe99f4
Vishwajeet Raj <vishwajeetraj11@gmail.com>
2021-08-15 13:48:45 +0530

    MUI overide, custom scroll bar

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,24 +1,27 @@
-import { StylesProvider } from '@material-ui/core';
+import { StylesProvider, ThemeProvider } from '@material-ui/core';
 import { Auth0ProviderWithHistory } from 'auth/auth0-provider-with-history';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
+import { theme } from 'shared/theme';
 import { store } from 'store/store';
 import { App } from './App';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
-  <StylesProvider injectFirst>
-    <Provider store={store}>
-      <BrowserRouter>
-        <Auth0ProviderWithHistory>
-          <App />
-        </Auth0ProviderWithHistory>
-      </BrowserRouter>
-    </Provider>
-  </StylesProvider>,
+  <ThemeProvider theme={theme}>
+    <StylesProvider injectFirst>
+      <Provider store={store}>
+        <BrowserRouter>
+          <Auth0ProviderWithHistory>
+            <App />
+          </Auth0ProviderWithHistory>
+        </BrowserRouter>
+      </Provider>
+    </StylesProvider>
+  </ThemeProvider>,
   document.getElementById('root')
 );
 
