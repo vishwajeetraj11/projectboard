9238dcad38951be3ba0b63bd03ba057f3c2f14f6
Vishwajeet Raj <vishwajeetraj11@gmail.com>
2021-08-20 14:55:41 +0530

    close task filter modal on select filter

--- client/src/components/modals/TaskFilterModal.tsx
+++ client/src/components/modals/TaskFilterModal.tsx
@@ -27,11 +27,11 @@ interface SearchOptionProps {
 
 interface Props {
   isOpen: boolean;
-  onDismiss?: () => void;
+  onDismiss: () => void;
 }
 
 
-function SearchOption({ name,onClick,Icon }: SearchOptionProps) {
+function SearchOption({ name, onClick, Icon }: SearchOptionProps) {
   return (
     <div onClick={onClick} className='flex items-center px-4 py-5 font-normal text-gray-700 border-l-2 border-transparent cursor-default text-14 hover:text-gray-800 hover:border-indigo-700 hover:bg-gray-100'>
       {Icon}
@@ -50,77 +50,77 @@ export const TaskFilterModal = ({ isOpen, onDismiss }: Props) => {
   };
 
   const options = [
-  {
-    name: 'Filter by status: To Do',
-    Icon: <TodoIcon className='w-4 h-4 mr-4 ' />,
-    onClick: () => dispatch(addStatusFilter(Status.TODO))
-  },
-  {
-    name: 'Filter by status: Backlog',
-    Icon: <BacklogIcon className='w-4 h-4 mr-4 ' />,
-    onClick: () => dispatch(addStatusFilter(Status.BACKLOG))
-  },
-  {
-    name: 'Filter by status: In Progress',
-    Icon: <InProgressIcon className='w-4 h-4 mr-4 ' />,
-    onClick: () => dispatch(addStatusFilter(Status.IN_PROGRESS))
-  },
-  {
-    name: 'Filter by status: Done',
-    Icon: <DoneIcon className='w-4 h-4 mr-4 ' />,
-    onClick: () => dispatch(addStatusFilter(Status.DONE))
-  },
-  {
-    name: 'Filter by status: Cancelled',
-    Icon: <CancelIcon className='w-4 h-4 mr-4 ' />,
-    onClick: () => dispatch(addStatusFilter(Status.CANCELED))
-  },
-  {
-    name: 'Filter by priority: No Priority',
-    Icon: <NoPriorityIcon className='w-4 h-4 mr-4 ' />,
-    onClick: () => dispatch(addPriorityFilter(Priority.NO_PRIORITY))
-  },
-  {
-    name: 'Filter by priority: Urgent',
-    Icon: <UrgentPriorityIcon className='w-4 h-4 mr-4 ' />,
-    onClick: () => dispatch(addPriorityFilter(Priority.URGENT))
-  },
-  {
-    name: 'Filter by priority: High',
-    Icon: <HighPriorityIcon className='w-4 h-4 mr-4 ' />,
-    onClick: () => dispatch(addPriorityFilter(Priority.HIGH))
-  },
-  {
-    name: 'Filter by priority: Medium',
-    Icon: <MediumPriorityIcon className='w-4 h-4 mr-4 ' />,
-    onClick: () => dispatch(addPriorityFilter(Priority.MEDIUM))
-  },
-  {
-    name: 'Filter by priority: Low',
-    Icon: <LowPriorityIcon className='w-4 h-4 mr-4 ' />,
-    onClick: () => dispatch(addPriorityFilter(Priority.LOW))
-  },
-  {
-    name: 'Filter by labels: Bug',
-    Icon: <div className="w-2.5 h-2.5 rounded-full mr-3" style={{ background: '#eb5757' }}></div>,
-    onClick: () => dispatch(addLabelFilter(Labels.BUG))
-  },
-  {
-    name: 'Filter by labels: Feature',
-    Icon: <div className="w-2.5 h-2.5 rounded-full mr-3" style={{ background: '#bb87fc' }}></div>,
-    onClick: () => dispatch(addLabelFilter(Labels.FEATURE))
-  },
-  {
-    name: 'Filter by labels: Improvement',
-    Icon: <div className="w-2.5 h-2.5 rounded-full mr-3" style={{ background: '#4ea7fc' }}></div>,
-    onClick: () => dispatch(addLabelFilter(Labels.IMPROVEMENT))
-  },
-  {
-    name: 'Filter by labels: No Label',
-    Icon: <div className="w-2.5 h-2.5 rounded-full mr-3" style={{ background: '#999999' }}></div>,
-    onClick: () => dispatch(addLabelFilter(Labels.NO_LABEL))
-  },
-];
+    {
+      name: 'Filter by status: To Do',
+      Icon: <TodoIcon className='w-4 h-4 mr-4 ' />,
+      onClick: () => { dispatch(addStatusFilter(Status.TODO)); onDismiss(); }
+    },
+    {
+      name: 'Filter by status: Backlog',
+      Icon: <BacklogIcon className='w-4 h-4 mr-4 ' />,
+      onClick: () => { dispatch(addStatusFilter(Status.BACKLOG)); onDismiss(); }
+    },
+    {
+      name: 'Filter by status: In Progress',
+      Icon: <InProgressIcon className='w-4 h-4 mr-4 ' />,
+      onClick: () => { dispatch(addStatusFilter(Status.IN_PROGRESS)); onDismiss(); }
+    },
+    {
+      name: 'Filter by status: Done',
+      Icon: <DoneIcon className='w-4 h-4 mr-4 ' />,
+      onClick: () => { dispatch(addStatusFilter(Status.DONE)); onDismiss(); }
+    },
+    {
+      name: 'Filter by status: Cancelled',
+      Icon: <CancelIcon className='w-4 h-4 mr-4 ' />,
+      onClick: () => { dispatch(addStatusFilter(Status.CANCELED)); onDismiss(); }
+    },
+    {
+      name: 'Filter by priority: No Priority',
+      Icon: <NoPriorityIcon className='w-4 h-4 mr-4 ' />,
+      onClick: () => { dispatch(addPriorityFilter(Priority.NO_PRIORITY)); onDismiss(); }
+    },
+    {
+      name: 'Filter by priority: Urgent',
+      Icon: <UrgentPriorityIcon className='w-4 h-4 mr-4 ' />,
+      onClick: () => { dispatch(addPriorityFilter(Priority.URGENT)); onDismiss(); }
+    },
+    {
+      name: 'Filter by priority: High',
+      Icon: <HighPriorityIcon className='w-4 h-4 mr-4 ' />,
+      onClick: () => { dispatch(addPriorityFilter(Priority.HIGH)); onDismiss(); }
+    },
+    {
+      name: 'Filter by priority: Medium',
+      Icon: <MediumPriorityIcon className='w-4 h-4 mr-4 ' />,
+      onClick: () => { dispatch(addPriorityFilter(Priority.MEDIUM)); onDismiss(); }
+    },
+    {
+      name: 'Filter by priority: Low',
+      Icon: <LowPriorityIcon className='w-4 h-4 mr-4 ' />,
+      onClick: () => { dispatch(addPriorityFilter(Priority.LOW)); onDismiss(); }
+    },
+    {
+      name: 'Filter by labels: Bug',
+      Icon: <div className="w-2.5 h-2.5 rounded-full mr-3" style={{ background: '#eb5757' }}></div>,
+      onClick: () => { dispatch(addLabelFilter(Labels.BUG)); onDismiss(); }
+    },
+    {
+      name: 'Filter by labels: Feature',
+      Icon: <div className="w-2.5 h-2.5 rounded-full mr-3" style={{ background: '#bb87fc' }}></div>,
+      onClick: () => { dispatch(addLabelFilter(Labels.FEATURE)); onDismiss(); }
+    },
+    {
+      name: 'Filter by labels: Improvement',
+      Icon: <div className="w-2.5 h-2.5 rounded-full mr-3" style={{ background: '#4ea7fc' }}></div>,
+      onClick: () => { dispatch(addLabelFilter(Labels.IMPROVEMENT)); onDismiss(); }
+    },
+    {
+      name: 'Filter by labels: No Label',
+      Icon: <div className="w-2.5 h-2.5 rounded-full mr-3" style={{ background: '#999999' }}></div>,
+      onClick: () => { dispatch(addLabelFilter(Labels.NO_LABEL)); onDismiss(); }
+    },
+  ];
 
   return (
     <Modal isOpen={isOpen} onDismiss={onDismiss} size='large' className='h-100' modalStyles={{ maxWidth: '700px', width: '100%' }}>
