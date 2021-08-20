f255405c029be1d151c8ffe4c1d9940d8fcc483c
Masoom Kumar <masoom.kr04@gmail.com>
2021-08-20 14:02:24 +0530

    rendered all filters

--- client/src/components/modals/TaskFilterModal.tsx
+++ client/src/components/modals/TaskFilterModal.tsx
@@ -4,12 +4,20 @@ import { ReactComponent as BacklogIcon } from 'assets/icons/circle-dot.svg';
 import { ReactComponent as TodoIcon } from 'assets/icons/circle.svg';
 import { ReactComponent as DoneIcon } from 'assets/icons/done.svg';
 import { ReactComponent as InProgressIcon } from 'assets/icons/half-circle.svg';
-
+import { ReactComponent as NoPriorityIcon } from 'assets/icons/dots.svg';
+import { ReactComponent as UrgentPriorityIcon } from 'assets/icons/rounded-claim.svg';
+import { ReactComponent as MediumPriorityIcon } from 'assets/icons/signal-medium.svg';
+import { ReactComponent as HighPriorityIcon } from 'assets/icons/signal-strong.svg';
+import { ReactComponent as LowPriorityIcon } from 'assets/icons/signal-weak.svg';
 import Modal from 'components/Modal';
 import React, { useState } from 'react';
 import { useDispatch } from 'react-redux';
 import { Status } from 'shared/constants';
+import { Priority } from 'shared/constants';
+import { Labels } from 'shared/constants';
 import { addStatusFilter } from 'store/actions/filterActions';
+import { addPriorityFilter } from 'store/actions/filterActions';
+import { addLabelFilter } from 'store/actions/filterActions';
 
 interface SearchOptionProps {
   name: string;
@@ -19,54 +27,100 @@ interface SearchOptionProps {
 
 interface Props {
   isOpen: boolean;
-  onDismiss: () => void;
+  onDismiss?: () => void;
 }
 
 
-function SearchOption({ name, onClick, Icon }: SearchOptionProps) {
+function SearchOption({ name,onClick,Icon }: SearchOptionProps) {
   return (
     <div onClick={onClick} className='flex items-center px-4 py-5 font-normal text-gray-700 border-l-2 border-transparent cursor-default text-14 hover:text-gray-800 hover:border-indigo-700 hover:bg-gray-100'>
       {Icon}
       <div className='text-overflow-ellipsis flex-nowrap text-14'>{name}</div>
     </div>
   );
-}
+};
 
 
 export const TaskFilterModal = ({ isOpen, onDismiss }: Props) => {
   const [search, setSearch] = useState('');
   const dispatch = useDispatch();
-  const [options,] = useState([
-    {
-      name: 'Filter by status: To Do',
-      Icon: <TodoIcon className='w-4 h-4 mr-4 ' />,
-      onClick: () => { dispatch(addStatusFilter(Status.TODO)); onDismiss(); }
-    },
-    {
-      name: 'Filter by status: Backlog',
-      Icon: <BacklogIcon className='w-4 h-4 mr-4 ' />,
-      onClick: () => { dispatch(addStatusFilter(Status.BACKLOG)); onDismiss(); }
-    },
-    {
-      name: 'Filter by status: In Progress',
-      Icon: <InProgressIcon className='w-4 h-4 mr-4 ' />,
-      onClick: () => { dispatch(addStatusFilter(Status.IN_PROGRESS)); onDismiss(); }
-    },
-    {
-      name: 'Filter by status: Done',
-      Icon: <DoneIcon className='w-4 h-4 mr-4 ' />,
-      onClick: () => { dispatch(addStatusFilter(Status.DONE)); onDismiss(); }
-    },
-    {
-      name: 'Filter by status: Cancelled',
-      Icon: <CancelIcon className='w-4 h-4 mr-4 ' />,
-      onClick: () => { dispatch(addStatusFilter(Status.CANCELED)); onDismiss(); }
-    },
-  ]);
 
   const onSearchChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setSearch(e.target.value);
   };
+
+  const options = [
+  {
+    name: 'Filter by status: To Do',
+    Icon: <TodoIcon className='w-4 h-4 mr-4 ' />,
+    onClick: () => dispatch(addStatusFilter(Status.TODO))
+  },
+  {
+    name: 'Filter by status: Backlog',
+    Icon: <BacklogIcon className='w-4 h-4 mr-4 ' />,
+    onClick: () => dispatch(addStatusFilter(Status.BACKLOG))
+  },
+  {
+    name: 'Filter by status: In Progress',
+    Icon: <InProgressIcon className='w-4 h-4 mr-4 ' />,
+    onClick: () => dispatch(addStatusFilter(Status.IN_PROGRESS))
+  },
+  {
+    name: 'Filter by status: Done',
+    Icon: <DoneIcon className='w-4 h-4 mr-4 ' />,
+    onClick: () => dispatch(addStatusFilter(Status.DONE))
+  },
+  {
+    name: 'Filter by status: Cancelled',
+    Icon: <CancelIcon className='w-4 h-4 mr-4 ' />,
+    onClick: () => dispatch(addStatusFilter(Status.CANCELED))
+  },
+  {
+    name: 'Filter by priority: No Priority',
+    Icon: <NoPriorityIcon className='w-4 h-4 mr-4 ' />,
+    onClick: () => dispatch(addPriorityFilter(Priority.NO_PRIORITY))
+  },
+  {
+    name: 'Filter by priority: Urgent',
+    Icon: <UrgentPriorityIcon className='w-4 h-4 mr-4 ' />,
+    onClick: () => dispatch(addPriorityFilter(Priority.URGENT))
+  },
+  {
+    name: 'Filter by priority: High',
+    Icon: <HighPriorityIcon className='w-4 h-4 mr-4 ' />,
+    onClick: () => dispatch(addPriorityFilter(Priority.HIGH))
+  },
+  {
+    name: 'Filter by priority: Medium',
+    Icon: <MediumPriorityIcon className='w-4 h-4 mr-4 ' />,
+    onClick: () => dispatch(addPriorityFilter(Priority.MEDIUM))
+  },
+  {
+    name: 'Filter by priority: Low',
+    Icon: <LowPriorityIcon className='w-4 h-4 mr-4 ' />,
+    onClick: () => dispatch(addPriorityFilter(Priority.LOW))
+  },
+  {
+    name: 'Filter by labels: Bug',
+    Icon: <div className="w-2.5 h-2.5 rounded-full mr-3" style={{ background: '#eb5757' }}></div>,
+    onClick: () => dispatch(addLabelFilter(Labels.BUG))
+  },
+  {
+    name: 'Filter by labels: Feature',
+    Icon: <div className="w-2.5 h-2.5 rounded-full mr-3" style={{ background: '#bb87fc' }}></div>,
+    onClick: () => dispatch(addLabelFilter(Labels.FEATURE))
+  },
+  {
+    name: 'Filter by labels: Improvement',
+    Icon: <div className="w-2.5 h-2.5 rounded-full mr-3" style={{ background: '#4ea7fc' }}></div>,
+    onClick: () => dispatch(addLabelFilter(Labels.IMPROVEMENT))
+  },
+  {
+    name: 'Filter by labels: No Label',
+    Icon: <div className="w-2.5 h-2.5 rounded-full mr-3" style={{ background: '#999999' }}></div>,
+    onClick: () => dispatch(addLabelFilter(Labels.NO_LABEL))
+  },
+];
 
   return (
     <Modal isOpen={isOpen} onDismiss={onDismiss} size='large' className='h-100' modalStyles={{ maxWidth: '700px', width: '100%' }}>
@@ -84,3 +138,5 @@ export const TaskFilterModal = ({ isOpen, onDismiss }: Props) => {
     </Modal>
   );
 };
+
+
