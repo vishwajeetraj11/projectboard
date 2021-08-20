25e32a6277247445bca2b6946e66b1cf2828a80d
Vishwajeet Raj <vishwajeetraj11@gmail.com>
2021-08-20 13:19:01 +0530

    only admin rendering in manage members

--- client/src/components/members/MemberRow.tsx
+++ client/src/components/members/MemberRow.tsx
@@ -2,6 +2,8 @@ import { Button } from '@material-ui/core';
 import React from 'react';
 import { Member } from 'shared/types';
 import HighlightOffOutlinedIcon from '@material-ui/icons/HighlightOffOutlined';
+import { useSelector } from 'react-redux';
+import { RootState } from 'store/store';
 
 interface Props {
   member: Member,
@@ -11,6 +13,7 @@ interface Props {
 }
 
 export const MemberRow: React.FC<Props> = ({ member, isMobile, disableDeleteButton, onDeleteMember }) => {
+  const { projectData } = useSelector((state: RootState) => state.currentProject);
   const onDelete = () => {
     onDeleteMember(member._id);
   };
@@ -19,7 +22,7 @@ export const MemberRow: React.FC<Props> = ({ member, isMobile, disableDeleteButt
       <p>{member.user.firstName} {member.user.lastName}</p>
       <p>{member.access}</p>
       {!isMobile && <p>{member.user.email}</p>}
-      {!(member.access === 'admin') && <Button onClick={onDelete} disabled={disableDeleteButton} className='flex items-center justify-center'><HighlightOffOutlinedIcon className='h-5 w-5 text-red-500' /></Button>}
+      {(projectData.access === 'admin' && !(member.access === 'admin')) && <Button onClick={onDelete} disabled={disableDeleteButton} className='flex items-center justify-center'><HighlightOffOutlinedIcon className='h-5 w-5 text-red-500' /></Button>}
     </div>
   );
 };
