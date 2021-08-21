3edfb72789a9f5eed6e65ec56b54d00a7dd9f022
Vishwajeet Raj <vishwajeetraj11@gmail.com>
2021-08-21 23:16:10 +0530

    Top Filter Handle Responsivity.

--- client/src/components/TopFilter.tsx
+++ client/src/components/TopFilter.tsx
@@ -22,7 +22,7 @@ export const TopFilter = ({ title, onOpenMenu, type }: Props) => {
   const dispatch = useDispatch();
 
   // const tasks = useSelector((state: RootState) => state.taskList.tasks);
-  const { status,priority,label } = useSelector((state: RootState) => state.filters);
+  const { status, priority, label } = useSelector((state: RootState) => state.filters);
   // const totaltasks = tasks.backlog.length + tasks.todo.length
   //   + tasks.done.length + tasks.in_progress.length + tasks.cancelled.length;
 
@@ -32,32 +32,32 @@ export const TopFilter = ({ title, onOpenMenu, type }: Props) => {
 
   return (
     <>
-      <div className='flex justify-between flex-shrink-0 pl-2 pr-6 border-b border-gray-200 h-14 lg:pl-9'>
+      <div className='flex justify-between flex-shrink-0 pl-2 pr-6 border-b border-gray-200 py-3 h-22 lg:h-18 lg:py-5 lg:pl-9'>
         {/* left section */}
-        <div className='flex items-center'>
+        <div className='flex items-center flex-wrap'>
           <button
-            className='flex-shrink-0 h-full px-5 focus:outline-none lg:hidden'
+            className='flex-shrink-0 px-5 focus:outline-none lg:hidden mt-2 lg:mt-0'
             onClick={onOpenMenu}
           ><MenuIcon className='w-3.5 text-gray-500 hover:text-gray-800' /></button>
 
           <div className='p-1 font-semibold cursor-default hover:bg-gray-100'>{title}</div>
           {/* {!onInvitePage && <span>{totaltasks}</span>} */}
           {onTasksPage && <>
-            {status && <div className='ml-3 capitalize text-gray-700 px-2 py-1 bg-gray-100 hover:bg-gray-50 rounded-md flex items-center'>
+            {status && <div className='mt-2 lg:mt-0 ml-3 capitalize text-gray-700 px-2 py-1 bg-gray-100 hover:bg-gray-50 rounded-md flex items-center'>
               <p>{getStatusText(status)}</p>
               <div onClick={() => dispatch(addStatusFilter(''))}><CloseIcon className='ml-1.5 w-3 h-3 cursor-pointer' /></div>
             </div>}
-            {priority && <div className='ml-3 capitalize text-gray-700 px-2 py-1 bg-gray-100 hover:bg-gray-50 rounded-md flex items-center'>
+            {priority && <div className='mt-2 lg:mt-0 ml-3 capitalize text-gray-700 px-2 py-1 bg-gray-100 hover:bg-gray-50 rounded-md flex items-center'>
               <p>{getPriorityString(priority)}</p>
               <div onClick={() => dispatch(addPriorityFilter(''))}><CloseIcon className='ml-1.5 w-3 h-3 cursor-pointer' /></div>
             </div>}
-            {label && <div className='ml-3 capitalize text-gray-700 px-2 py-1 bg-gray-100 hover:bg-gray-50 rounded-md flex items-center'>
+            {label && <div className='mt-2 lg:mt-0 ml-3 capitalize text-gray-700 px-2 py-1 bg-gray-100 hover:bg-gray-50 rounded-md flex items-center'>
               <p>{getLabelObj(label).name}</p>
               <div onClick={() => dispatch(addLabelFilter(''))}><CloseIcon className='ml-1.5 w-3 h-3 cursor-pointer' /></div>
             </div>}
           </>}
           {onTasksPage && <button
-            className='px-1 py-0.5 ml-3 border border-gray-300 border-dashed rounded text-gray-500 hover:border-gray-400 focus:outline-none hover:text-gray-800'
+            className='mt-2 lg:mt-0 px-1 py-0.5 ml-3 border border-gray-300 border-dashed rounded text-gray-500 hover:border-gray-400 focus:outline-none hover:text-gray-800'
             onClick={() => setShowFilter(!showFilter)}
           >+ Filter</button>}
         </div>
