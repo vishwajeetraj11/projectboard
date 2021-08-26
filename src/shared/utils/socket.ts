3891db64d6466291da0c6ec5b7d6636e2721ed33
Vishwajeet Raj <vishwajeetraj11@gmail.com>
2021-08-27 00:30:06 +0530

    trigger redeploy

--- src/shared/utils/socket.ts
+++ src/shared/utils/socket.ts
@@ -5,7 +5,7 @@ socket.emit('new_board_status_change', { a: "a", b: "b" });
 // console.log(socket);
 socket.on('connect_error', (err: any) => {
   console.log(err);
-  console.log(`connect_error due to ${err.message}`);
+  console.log(`connect_error due to: ${err.message}`);
 });
 
 export default socket;
