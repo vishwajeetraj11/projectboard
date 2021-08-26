b24cd4ffe6eab99f91f7860515287fa47a03d18b
Vishwajeet Raj <vishwajeetraj11@gmail.com>
2021-08-27 00:22:13 +0530

    socket url update

--- src/shared/utils/socket.ts
+++ src/shared/utils/socket.ts
@@ -1,6 +1,6 @@
 import io from 'socket.io-client';
 let socket: any;
-socket = io('http://localhost:3000', { transports: ['websocket'] });
+socket = io('https://productboard-backend.herokuapp.com/', { transports: ['websocket'] });
 socket.emit('new_board_status_change', { a: "a", b: "b" });
 // console.log(socket);
 socket.on('connect_error', (err: any) => {
