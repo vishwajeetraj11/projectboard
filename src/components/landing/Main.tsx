9dfb8f8d8cdee547ad6ed3eb7e1d218789b57712
Masoom Kumar <masoom.kr04@gmail.com>
2021-08-27 18:09:42 +0530

    static data

--- src/components/landing/Main.tsx
+++ src/components/landing/Main.tsx
@@ -9,10 +9,17 @@ interface Props {
 export const Main: React.FC<Props> = () => {
   return (
     <section className="dark:bg-coolGray-800 dark:text-coolGray-100">
-      <div className="container mx-auto flex flex-col items-center px-4 pt-12 text-center md:pt-16 md:px-10 lg:px-32 lg:mb-16 xl:max-w-3xl">
+      <div className="mx-auto flex flex-col items-center px-4 pt-12 text-center md:pt-16 md:px-10 lg:px-32 lg:mb-16 xl:max-w-3xl">
         <h1 className="max-w-8xl text-5xl font-black mx-0 mt-0 mb-5">ProductBoard</h1>
         <p className="px-8 mt-4 mb-8 text-lg text-gray-500">Cupiditate minima voluptate temporibus quia? Architecto beatae esse ab amet vero eaque explicabo!</p>
-        <button className="px-8 py-3 mb-4 text-lg bg-primary text-white font-semibold rounded dark:bg-violet-400 dark:text-coolGray-900">Get started</button>
+        {/* <button className="px-8 py-3 mb-4 text-lg bg-primary text-white font-semibold rounded dark:bg-violet-400 dark:text-coolGray-900">Get started</button> */}
+        <a href="#" className="px-8 py-3 relative rounded group font-medium text-white font-semibold inline-block">
+          <span className="absolute top-0 left-0 w-full h-full rounded opacity-50 filter blur-sm bg-gradient-to-br from-secondary to-primary"></span>
+          <span className="h-full w-full inset-0 absolute mt-0.5 ml-0.5 bg-gradient-to-br filter group-active:opacity-0 rounded opacity-50 from-secondary to-primary"></span>
+          <span className="absolute inset-0 w-full h-full transition-all duration-200 ease-out rounded shadow-xl bg-gradient-to-br filter group-active:opacity-0 group-hover:blur-sm from-secondary to-primary"></span>
+          <span className="absolute inset-0 w-full h-full transition duration-200 ease-out rounded bg-gradient-to-br to-secondary from-primary"></span>
+          <span className="relative">Get Started</span>
+        </a>
       </div>
       <div>
         <img src="https://images.unsplash.com/photo-1507099985932-87a4520ed1d5?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1050&q=80" alt="" className="w-4/6 mx-auto mb-12 mt-4 rounded-lg shadow-md"></img>
--- src/shared/staticData.tsx
+++ src/shared/staticData.tsx
@@ -1,142 +1,140 @@
 export const features = [
-    {
-      id: '1',
-      Icon: (
-        <svg
-          fill="none"
-          stroke="currentColor"
-          strokeLinecap="round"
-          strokeLinejoin="round"
-          strokeWidth="2"
-          className="w-6 h-6"
-          viewBox="0 0 24 24"
-        >
-          <path d="M20 21v-2a4 4 0 00-4-4H8a4 4 0 00-4 4v2" />
-          <circle cx="12" cy="7" r="4" />
-        </svg>
-      ),
-      title: 'Sign Up',
-      description:
-        'Signing up to OS Resume is even more cnetwme and email address. We keep your data strictly confidential.',
-      // description: 'Pick one of our free resume templates that fits your description. Beautiful layouts, pick your favorite.',
-    },
-    {
-      id: '2',
-      Icon: (
-        <svg
-          fill="none"
-          stroke="currentColor"
-          strokeLinecap="round"
-          strokeLinejoin="round"
-          strokeWidth="2"
-          className="w-6 h-6"
-          viewBox="0 0 24 24"
-        >
-          <circle cx="6" cy="6" r="3" />
-          <circle cx="6" cy="18" r="3" />
-          <path d="M20 4L8.12 15.88M14.47 14.48L20 20M8.12 8.12L12 12" />
-        </svg>
-      ),
-      title: 'Fill the Template',
-      description:
-        'Choose one of will find the relevent forms in the Sidebars. Every Sidebar wiete/rearrange information.',
-    },
-    {
-      id: '3',
-      Icon: (
-        <svg
-          fill="none"
-          stroke="currentColor"
-          strokeLinecap="round"
-          strokeLinejoin="round"
-          strokeWidth="2"
-          className="w-6 h-6"
-          viewBox="0 0 24 24"
-        >
-          <path d="M22 12h-4l-3 9L9 3l-3 9H2" />
-        </svg>
-      ),
-      title: 'Export your Resume',
-      description:
-        'Export your Resume to PDF. PDF  and most consistent visuaupdate/delete/rearrange information).',
-    },
-    {
-      id: '4',
-      Icon: (
-        <svg
-          fill="none"
-          stroke="currentColor"
-          strokeLinecap="round"
-          strokeLinejoin="round"
-          strokeWidth="2"
-          className="w-6 h-6"
-          viewBox="0 0 24 24"
-        >
-          <path d="M20 21v-2a4 4 0 00-4-4H8a4 4 0 00-4 4v2" />
-          <circle cx="12" cy="7" r="4" />
-        </svg>
-      ),
-      title: 'Sign Up',
-      description:
-        'Signing up ip this step and enter your name and email address. We keep your data strictly confidential.',
-      // description: 'Pick one of our free resume templates that fits your description. Beautiful layouts, pick your favorite.',
-    },
-    {
-      id: '5',
-      Icon: (
-        <svg
-          fill="none"
-          stroke="currentColor"
-          strokeLinecap="round"
-          strokeLinejoin="round"
-          strokeWidth="2"
-          className="w-6 h-6"
-          viewBox="0 0 24 24"
-        >
-          <circle cx="6" cy="6" r="3" />
-          <circle cx="6" cy="18" r="3" />
-          <path d="M20 4L8.12 15.88M14.47 14.48L20 20M8.12 8.12L12 12" />
-        </svg>
-      ),
-      title: 'Fill the Template',
-      description:
-        'Choose one of the temppen a drawer where you will have options to create/update/delete/rearrange information.',
-    },
-    {
-      id: '6',
-      Icon: (
-        <svg
-          fill="none"
-          stroke="currentColor"
-          strokeLinecap="round"
-          strokeLinejoin="round"
-          strokeWidth="2"
-          className="w-6 h-6"
-          viewBox="0 0 24 24"
-        >
-          <path d="M22 12h-4l-3 9L9 3l-3 9H2" />
-        </svg>
-      ),
-      title: 'Export your Resume',
-      description:
-        'Export your Resume to PDF is saved at every checkpoint (create/update/delete/rearrange information).',
-    },
-  ];
+  {
+    id: '1',
+    Icon: (
+      <svg
+        fill="none"
+        stroke="currentColor"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth="2"
+        className="w-6 h-6"
+        viewBox="0 0 24 24"
+      >
+        <path d="M20 21v-2a4 4 0 00-4-4H8a4 4 0 00-4 4v2" />
+        <circle cx="12" cy="7" r="4" />
+      </svg>
+    ),
+    title: 'Sign Up',
+    description:
+      'Signing Up using auth0 service is as easy it gets. Your data is completely safe.',
+  },
+  {
+    id: '2',
+    Icon: (
+      <svg
+        fill="none"
+        stroke="currentColor"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth="2"
+        className="w-6 h-6"
+        viewBox="0 0 24 24"
+      >
+        <circle cx="6" cy="6" r="3" />
+        <circle cx="6" cy="18" r="3" />
+        <path d="M20 4L8.12 15.88M14.47 14.48L20 20M8.12 8.12L12 12" />
+      </svg>
+    ),
+    title: 'Create projects',
+    description:
+      'Create projects for your teams or become a member on a project of other member.',
+  },
+  {
+    id: '3',
+    Icon: (
+      <svg
+        fill="none"
+        stroke="currentColor"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth="2"
+        className="w-6 h-6"
+        viewBox="0 0 24 24"
+      >
+        <path d="M22 12h-4l-3 9L9 3l-3 9H2" />
+      </svg>
+    ),
+    title: 'Work together',
+    description:
+      'As you work on a big project keep a track of all the changes using history tab.',
+  },
+  {
+    id: '4',
+    Icon: (
+      <svg
+        fill="none"
+        stroke="currentColor"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth="2"
+        className="w-6 h-6"
+        viewBox="0 0 24 24"
+      >
+        <path d="M20 21v-2a4 4 0 00-4-4H8a4 4 0 00-4 4v2" />
+        <circle cx="12" cy="7" r="4" />
+      </svg>
+    ),
+    title: 'Specificity',
+    description:
+      'Assign roles and priorities as they suit schedule and member availabilty in a click.',
+  },
+  {
+    id: '5',
+    Icon: (
+      <svg
+        fill="none"
+        stroke="currentColor"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth="2"
+        className="w-6 h-6"
+        viewBox="0 0 24 24"
+      >
+        <circle cx="6" cy="6" r="3" />
+        <circle cx="6" cy="18" r="3" />
+        <path d="M20 4L8.12 15.88M14.47 14.48L20 20M8.12 8.12L12 12" />
+      </svg>
+    ),
+    title: 'Filter tasks',
+    description:
+      'Using our multi layer filter, keep a track of tasks based on multiple fields.',
+  },
+  {
+    id: '6',
+    Icon: (
+      <svg
+        fill="none"
+        stroke="currentColor"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth="2"
+        className="w-6 h-6"
+        viewBox="0 0 24 24"
+      >
+        <path d="M22 12h-4l-3 9L9 3l-3 9H2" />
+      </svg>
+    ),
+    title: 'Instant connect',
+    description:
+      'Establish a connection with our community and talk to us directly regarding your queries.',
+  },
+];
 
-  export const Faqs = [
-    {
-      id: '1',
-      question: 'Why choose a Resume Builder ?',
-      ans: 'A Resume Builder helps you create a resume that’s professional, with an up-to-date design. You’ll make a good first impression on a future employer because your resume is attractive and readable.',
-    },
-    {
-      id: '2',
-      question: 'Is it free ?',
-      ans: 'Yes, It is free. Many resume generators are free to use.',
-    },
-    {
-      id: '3',
-      question: 'I can’t finish my resume right now. Will you save what I’ve done?',
-      ans: 'Yes, if you can’t finish your resume right now, we’ll save what you’ve done so can you finish later. If for any reason you need to close your browser tab or navigate away from the page, as long as you’ve clicked Submti after filling out a given section, OS Resume will have automatically saved your work so you can pick up where you left off.',
-    },
-  ];
+export const Faqs = [
+  {
+    id: '1',
+    question: 'Why another management app ?',
+    ans: 'Its simple. To remove all the hassle of managing teams and projects with multiple people aboard.',
+  },
+  {
+    id: '2',
+    question: 'Task managment would be problematic.',
+    ans: 'No, it’s not. Our app comes with advanced filters to separate tasks based on different fronts.',
+  },
+  {
+    id: '3',
+    question: 'We have got a big team. Can your app handle it ?',
+    ans: 'Yes, no problem. Our app provides all the features required to bring a big team together or manage existing ones.',
+  },
+];
