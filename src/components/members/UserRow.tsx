8b264718b0cbecaba7f18a1af587d27b93b86de1
Vishwajeet Raj <vishwajeetraj11@gmail.com>
2021-08-29 11:12:46 +0530

    show photo in add members, show assignee in task row

--- src/components/members/UserRow.tsx
+++ src/components/members/UserRow.tsx
@@ -1,7 +1,8 @@
 import { Button } from '@material-ui/core';
+import AddIcon from '@material-ui/icons/Add';
+import { Avatar } from 'components/Avatar';
 import React from 'react';
 import { User } from 'shared/types';
-import AddIcon from '@material-ui/icons/Add';
 
 interface Props {
   user: User;
@@ -15,7 +16,7 @@ interface Props {
 export const UserRow: React.FC<Props> = ({ isMobile, user, disableAddButton, onAddMember }) => {
   return (
     <div className='w-full member-grid-container my-3'>
-      <p className=''>Photo</p>
+      <Avatar name={`${user?.firstName} ${user?.lastName}`} />
       {!isMobile && <p>{`${user.firstName} ${user.lastName}`}</p>}
       <p className=''>{user.username}</p>
       <Button onClick={() => onAddMember(user._id)} disabled={disableAddButton} className='flex items-center justify-center bg-green-400'><AddIcon className='h-5 w-5 text-white' /></Button>
