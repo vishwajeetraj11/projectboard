c86b8c6baf3494e3cd8db42e76a57c0812ec203d
Vishwajeet Raj <vishwajeetraj11@gmail.com>
2021-09-14 21:36:16 +0530

    bug-fix: zIndex fix overlaping sidebars and status portals

--- src/components/LeftSideBar.tsx
+++ src/components/LeftSideBar.tsx
@@ -73,7 +73,7 @@ export const LeftSideBar: React.FC<Props> = ({ showMenu, onCloseMenu }) => {
 
   return (
     <>
-      <div className={classes} style={{ zIndex: 1000 }} ref={ref}>
+      <div className={classes} style={{ zIndex: 2 }} ref={ref}>
         <button
           className='flex-shrink-0 px-5 ml-2 lg:hidden h-14 focus:outline-none'
           onClick={onCloseMenu}
