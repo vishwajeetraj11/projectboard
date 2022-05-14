efde9f6b05196a6488542ba62b13949bc5bdc9f0
Vishwajeet Raj <ext-vishwajeet.raj@graphy.com>
2022-05-14 14:33:44 +0530

    depl

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,29 @@
-import "animate.css/animate.min.css";
+import 'animate.css/animate.min.css';
 import { cssTransition, ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import { Routes } from 'routes';
 
 const slideUp = cssTransition({
-  enter: "animate__animated animate__slideInUp",
-  exit: "animate__animated animate__slideOutDown"
+    enter: 'animate__animated animate__slideInUp',
+    exit: 'animate__animated animate__slideOutDown'
 });
 
 export const App = () => {
-
-  return (
-    <div className="App">
-      <Routes />
-      <ToastContainer
-        position="bottom-right"
-        autoClose={5000}
-        hideProgressBar
-        newestOnTop
-        closeOnClick
-        rtl={false}
-        transition={slideUp}
-        pauseOnFocusLoss
-        draggable
-        pauseOnHover
-      />
-    </div>
-  );
+    return (
+        <div className="App">
+            <Routes />
+            <ToastContainer
+                position="bottom-right"
+                autoClose={5000}
+                hideProgressBar
+                newestOnTop
+                closeOnClick
+                rtl={false}
+                transition={slideUp}
+                pauseOnFocusLoss
+                draggable
+                pauseOnHover
+            />
+        </div>
+    );
 };
-
-
