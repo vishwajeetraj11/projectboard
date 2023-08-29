2efcfcf32ec983c8ed0869b4e442bb0cd987da3d
Vishwajeet Raj <vishwajeetraj11@gmail.com>
2023-08-30 01:18:26 +0530

    Update socket.ts

--- src/shared/utils/socket.ts
+++ src/shared/utils/socket.ts
@@ -1,6 +1,6 @@
 import io from 'socket.io-client';
 let socket: any;
-socket = io('https://projectboard-backend.herokuapp.com/', { transports: ['websocket'] });
+socket = io('https://projectboard.onrender.com', { transports: ['websocket'] });
 socket.on('connect_error', (err: any) => {
   console.log(err);
   console.log(`Connection Error due to: ${err.message}`);
